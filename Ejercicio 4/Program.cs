5aa33b62c7313d5e44b00e304db3201e424f56b2
dgTr3 <Edugta31@gmail.com>
2026-03-21 17:59:09 -0600

    Ejercicio 4 Completo

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Año de nacimineto");
+int year = 0;
+int actual= DateTime.Now.Year;
+bool valido=false;
+while(!valido)
+{
+    Console.WriteLine("Ingrese año de nacimiento: ");
+    valido=int.TryParse(Console.ReadLine(), out year) && year > 1900 && year <= actual;
+    if (valido)
+    {
+        Console.WriteLine("Ingrese un año valido");
+    }
+
+}
+Console.WriteLine("Año valido: "+ year);
