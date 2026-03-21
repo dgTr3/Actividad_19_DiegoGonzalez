c1288fda629b3266d54f64132a7ad8864f7cbaa4
dgTr3 <Edugta31@gmail.com>
2026-03-21 17:55:20 -0600

    Ejercicio 3 completo

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Estudiantes");
+Console.WriteLine();
+int estudiantes = 0;
+bool valido = false;
+while (!valido)
+{
+    Console.WriteLine("Ingrese cantidad de estudiantes: ");
+    valido = int.TryParse(Console.ReadLine(), out estudiantes) && estudiantes > 0;
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número mayor a 0");
+    }
+}
+Console.WriteLine("Cantidad de estudientes: " + estudiantes );
