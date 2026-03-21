4f52902f4b8ef1149eead95567f5c0944441802c
dgTr3 <Edugta31@gmail.com>
2026-03-21 17:47:38 -0600

    ejercicio 1 completo

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Validar edad");
+Console.WriteLine();
+int edad;
+bool valido = false;
+while(!valido)
+{
+    Console.WriteLine("Ingrese la edad");
+    valido=int.TryParse(Console.ReadLine(), out edad);
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número ");
+    }
+
+}
+Console.WriteLine("Edad valida!");
