8707e8275ff7f0422c3d031a525819d091f0b14e
dgTr3 <Edugta31@gmail.com>
2026-03-21 17:51:04 -0600

    ejercicio 2 completo

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Precio de producto");
+Console.WriteLine();
+decimal precio;
+bool valido = false;
+while (!valido)
+{
+    Console.WriteLine("Ingrese precio: ");
+    valido = decimal.TryParse(Console.ReadLine(), out precio) && precio > 0;
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número mayor a 0");
+    }
+}
+Console.WriteLine("Precio valido");
