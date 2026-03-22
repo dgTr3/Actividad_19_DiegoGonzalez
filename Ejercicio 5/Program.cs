40cc43b1fe4cfc37ac7c126c4c5e6dccf40609a9
dgTr3 <Edugta31@gmail.com>
2026-03-21 18:01:41 -0600

    Ejercicio 5 Completo

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Número par o impar");
+int numero = 0;
+bool valido = false;
+while (!valido)
+{
+    Console.WriteLine("Ingrese un número: ");
+    valido = int.TryParse(Console.ReadLine(), out numero);
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número");
+    }
+}
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El número es par");
+}
+else
+{
+    Console.WriteLine("El número es impar");
+}
