baab85b7d19e9d1f0319de3bf0017d3ee731ee38
dgTr3 <Edugta31@gmail.com>
2026-03-21 19:55:26 -0600

    ejercicio 8 completo

---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Sacar raiz cuadrada");
+double numero=0;
+bool valido = false;
+while (!valido)
+{
+    Console.WriteLine("Ingrese un número: ");
+    valido = double.TryParse(Console.ReadLine(), out numero) && numero >= 0;
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número mayor o igual a 0");
+    }
+}
+double raiz = numero / 2;
+Console.WriteLine("La raiz cuadrada es: " + raiz );
