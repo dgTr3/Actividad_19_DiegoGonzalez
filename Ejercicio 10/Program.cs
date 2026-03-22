bd8e75c791472a2fb96bb7d3a27f586d401e0664
dgTr3 <Edugta31@gmail.com>
2026-03-21 20:09:42 -0600

    Ejercicio 10 completo

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Cantidad de productos");
+int cantidad =0;
+bool valido = false;    
+while (!valido)
+{
+    Console.WriteLine("Ingrese cantidad de productos: ");
+    valido = int.TryParse(Console.ReadLine(), out cantidad);
+    if (!valido || cantidad<=0)
+    {
+        Console.WriteLine("Debe ingresar un número mayor a 0");
+        valido = false;
+
+    }
+
+}
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.WriteLine("Ingrese el nombre del producto" + i  );
+    string producto = Console.ReadLine();   
+}
