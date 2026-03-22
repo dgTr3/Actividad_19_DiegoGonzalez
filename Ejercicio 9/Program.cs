a1cfb6131a74cd2cbb74673acda7961767a01e72
dgTr3 <Edugta31@gmail.com>
2026-03-21 19:58:18 -0600

    Ejercicio 9 completo

---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Nota de estudientes");
+int nota = 0;
+bool valido = false;
+while(valido==false)
+{
+    Console.WriteLine("Ingrese la nota del estudiante");
+    valido = int.TryParse(Console.ReadLine(), out nota);
+    if (!valido || nota < 0 || nota > 100)
+    {
+        Console.WriteLine("nota invalida");
+        valido = false;
+    }
+}
+if (nota >=60)
+{
+    Console.WriteLine("Aprueba");
+}
+else
+{
+    Console.WriteLine("Reprobado");
+}
