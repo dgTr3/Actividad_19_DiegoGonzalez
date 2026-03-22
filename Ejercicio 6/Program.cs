34a28cfccf9e7d51ad0d20fb5cc5c66b732bc5dd
dgTr3 <Edugta31@gmail.com>
2026-03-21 19:38:39 -0600

    Ejercicio 6 completo

---
+++ Ejercicio 6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Validar una fecha");
+Console.WriteLine();
+DateTime fecha = default;
+bool valido = false;
+while (!valido)
+{
+    Console.WriteLine("Ingrese una fecha (dd/MM/yyyy): ");
+    valido = DateTime.TryParse(Console.ReadLine(), out fecha);
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar una fecha en el formato dd/MM/yyyy");
+    }
+
+
+}
+Console.WriteLine("Fecha en formato corto: " + fecha);
