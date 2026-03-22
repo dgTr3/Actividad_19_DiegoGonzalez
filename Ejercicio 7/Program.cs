c55efbc2a4f33b09dc8562237eaefa67c1abe90d
dgTr3 <Edugta31@gmail.com>
2026-03-21 19:41:00 -0600

    Ejercicio 7 completo

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Salario de empleado");
+decimal salario = 0;    
+bool valido = false;
+while(!valido)
+{
+    Console.WriteLine("Ingrese salario");
+    valido=decimal.TryParse(Console.ReadLine(), out salario) && salario > 0;
+    if (!valido)
+    {
+        Console.WriteLine("Debe ingresar un número mayor a 0");
+    }
+}
+Console.WriteLine("Salario valido: " + salario);
